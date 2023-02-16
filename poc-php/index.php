6b10490364dcd8b088cff55b12eaeb64d813e605
Fabio Vige <fabiovige@gmail.com>
2023-02-16 15:59:41 -0300

    construct property promotion

--- poc-php/index.php
+++ poc-php/index.php
@@ -1,3 +1,24 @@
 <?php
 
-echo 'ola mundo';
+class FisrtService
+{
+    public function execute()
+    {
+        return 'first service running';
+    }
+}
+
+class SecondService
+{
+    public function __construct(
+        private FisrtService $service
+    ){}
+
+    public function hundle()
+    {
+        return $this->service->execute();
+    }
+}
+
+$secondService = new SecondService( new FisrtService());
+echo $secondService->hundle();
