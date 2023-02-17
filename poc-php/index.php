68402a494665590fbc6904d1c59f187820472221
Fabio Vige <fabiovige@gmail.com>
2023-02-17 06:11:10 -0300

    revisando-php

--- poc-php/index.php
+++ poc-php/index.php
@@ -1,20 +1,2 @@
 <?php
 
-class FisrtService
-{
-    public function execute()
-    {
-        return 'first service running';
-    }
-}
-
-class SecondService
-{
-    public function hundle(FisrtService $service)
-    {
-        return $service->execute();
-    }
-}
-
-$secondService = new SecondService();
-echo $secondService->hundle(new FisrtService);
