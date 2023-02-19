91139fa8592c50cc04e8f1d9e5c8b7f64548288f
Fabio Vige <fabiovige@gmail.com>
2023-02-19 10:09:53 -0300

    classes e objetos

--- poc-php/index.php
+++ poc-php/index.php
@@ -1,2 +1,13 @@
 <?php
 
+class Bank {
+    public $name;
+}
+
+$bankOne = new Bank();
+$bankOne->name = 'Nubank';
+echo $bankOne->name;
+
+$bankTwo = new Bank();
+$bankTwo->name = 'Inter';
+echo $bankTwo->name;
