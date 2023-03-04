58ad40f35bb6f80d406148bdcadef02d7edf12e8
Fabio Vige <fabiovige@gmail.com>
2023-03-04 10:44:39 -0300

    oop - encapsulamento

--- poc-php/index.php
+++ poc-php/index.php
@@ -1,2 +1,25 @@
 <?php
 
+class Bank {
+    private $name;
+    public $address;
+
+    public function setTax() 
+    {
+        return 10;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
+$bankOne = new Bank();
+$bankOne->setName('teste');
+echo $bankOne->getName();
