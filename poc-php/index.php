808274ef5e39b78be3b41467de0f1d87cca9b71f
Fabio Vige <fabiovige@gmail.com>
2023-03-04 10:56:02 -0300

    oop-heranca

--- poc-php/index.php
+++ poc-php/index.php
@@ -1,2 +1,25 @@
 <?php
 
+class Bank
+{
+    private $name;
+    protected $address;
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
+class NationalBank extends Bank {
+
+}
+
+$nationalBank = new NationalBank();
+$nationalBank->setName('XPTO');
+echo $nationalBank->getName();
