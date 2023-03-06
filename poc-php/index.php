3dd960a5c2967781f1436e9c29c846ce7735c851
Fabio Vige <fabiovige@gmail.com>
2023-03-06 10:16:09 -0300

    oop injecao de dependencia

--- poc-php/index.php
+++ poc-php/index.php
@@ -1,2 +1,32 @@
 <?php
 
+interface Money {
+    public function getMoney();
+}
+
+class Bank implements Money 
+{
+    public function getMoney()
+    {
+        echo 'teste 1';
+    }
+}
+
+class Broker implements Money 
+{
+    public function getMoney()
+    {
+        echo 'teste 2';
+    }
+}
+
+class OperationService
+{
+    public function retrieveInvestments(Money $institution)
+    {
+        return $institution->getMoney();
+    }
+}
+
+$service = new OperationService;
+echo $service->retrieveInvestments(new Bank);
