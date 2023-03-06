f861b53074873151c0022c2fcf1ef0d299932117
Fabio Vige <fabiovige@gmail.com>
2023-03-06 09:38:08 -0300

    teste

--- poc-php/index.php
+++ poc-php/index.php
@@ -1,2 +1,34 @@
 <?php
 
+class Bank {
+    public $balance;
+
+    public function getMoney()
+    {
+        return $this->balance;
+    }
+}
+
+class NationalBank extends Bank
+{
+    public function getMoney()
+    {
+        return 'R$ ' . $this->balance;
+    }
+}
+
+class InternationalBank extends Bank
+{
+    public function getMoney()
+    {
+        return 'US$ ' . $this->balance;
+    }
+}
+
+$bankNational = new NationalBank();
+$bankNational->balance = 20;
+
+$bankInternational = new InternationalBank();
+$bankInternational->balance = 20;
+
+echo $bankNational->getMoney() .' '. $bankInternational->getMoney();
